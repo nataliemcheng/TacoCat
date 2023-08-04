035c8e68266ae6dde8727477425abbc1e4da62c0
Natalie Cheng <nataliemcheng1@gmail.com>
2023-08-03 19:37:54 -0700

    Finished
    Finished Taco Cat

--- js/site.js
+++ js/site.js
@@ -1,4 +1,38 @@
-// Call Hello World
-function helloWorld() {
-    alert("Hello World");
+// controller function
+function getVals() {
+    document.getElementById("alert").classList.add("invisible");
+
+    let userInput = document.getElementById("userInput").value;
+    let returnObj = checkPalin(userInput);
+    display(returnObj);
+}
+
+// logic function
+function checkPalin(string) {
+    string = string.toLowerCase();
+    let regex = /[^a-z0-9]/gi;
+    string = string.replace(regex,"");
+
+    let revString = [];
+    let returnObj = {};
+
+    for (let i = string.length - 1; i >= 0; i--) {
+        revString += string[i];
+    }
+
+    if (revString == string) {
+        returnObj.msg = "Well Done! You entered a palindrome!";
+    } else {
+        returnObj.msg = "Sorry! You did not enter a palindrome!";
+    }
+
+    returnObj.reversed = revString;
+    return returnObj;
+}
+
+// view function
+function display(returnObj) {
+    document.getElementById("alertHeader").innerHTML = returnObj.msg;
+    document.getElementById("msg").innerHTML = `Your phrase reversed is: ${returnObj.reversed}`;
+    document.getElementById("alert").classList.remove("invisible");
 }
